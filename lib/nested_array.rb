72657357095c337104777137675b6faa7269a6ed
Phoenix R <phoenixrabang@gmail.com>
2020-01-19 02:12:46 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -48,8 +48,7 @@ end
 end
 
 def matrix_lookup(matrix, row, column)
-  # Given any matrix (array of arrays), a row index and a column index, 
-  # Return the matrix's content at that row and and column
+  matrix[rox][column]
 end
 
 def matrix_update(matrix, row, column, new_value)
